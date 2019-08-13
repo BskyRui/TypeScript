ab6046f3336a7d19ec11ce1b97006cf6493ccb66
LiRui <bskyrui@msn.cn>
2019-08-13 12:11:35 +0800

    update var.ts

--- 2_variable/var.ts
+++ 2_variable/var.ts
@@ -9,3 +9,41 @@ for (let i = 0; i < 10; i++) {
 // const 常量
 const h = 'ssss';
 // h = 'aaaa';  
+
+// 解构 展开
+let ipt: [number, number] = [0, 1];
+function f([first, second]: [number, number]) {
+    console.log(first, second);
+}
+
+f(ipt)
+
+let [fir, ...other] = [1, 2, 3, 4];
+// 1 [ 2, 3, 4 ]
+console.log(fir, other);
+
+// 对象解构
+// e({ a, b }?: { a: string; b?: number; }): void
+function e({a, b = 0} = {a: ''}): void {
+
+}
+e({a: 'yes'});
+e();
+
+// 数组展开
+let arr = ['a', 'b', 'c'];
+let otherArr = ['d', 'e'];
+console.log([...arr, ...otherArr]);
+
+// 对象展开
+let info = {
+    name: 'lr',
+    age: 22,
+    height: 172,
+};
+
+let newInfo = {
+    ...info,
+    name:  'LR'
+};
+console.log(newInfo);
