a9ae1134d14640f6697defd52fffbf939cded69d
LiRui <bskyrui@msn.cn>
2019-08-14 09:34:37 +0800

    add class example

---
+++ 4_class/class.ts
@@ -0,0 +1,39 @@
+class Animal {
+    name: string
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    move(distance: number = 0) {
+        console.log(`${this.name} moved distance ${distance}m`);
+    }
+}
+
+class Snake extends Animal {
+    constructor(name: string) {
+        super(name)
+    }
+
+    move(distance: number = 5) {
+        console.log('Slithering...');
+        super.move(distance);
+    }
+}
+
+class Horse extends Animal {
+    constructor(name: string) {
+        super(name)
+    }
+
+    move(distance: number = 45) {
+        console.log('Galloping...');
+        super.move(distance);
+    }
+}
+
+let t = new Snake('Tom');
+t.move();
+
+let sam: Animal = new Horse('Sam');
+sam.move(34);
