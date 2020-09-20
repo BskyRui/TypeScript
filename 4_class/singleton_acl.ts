ea7123403d00716a33daf6ed8a9fc46742a3c555
LiRui <bskyrui@msn.cn>
2020-09-20 11:39:18 +0800

    add acl decorator comments

--- 4_class/singleton_acl.ts
+++ 4_class/singleton_acl.ts
@@ -1,3 +1,7 @@
+// public: 公有属性，都可以访问到
+// protected: 在类里、子类里可以访问，类的外部无法访问
+// private: 私有属性，只能在自身使用，子类和类的外部无法使用
+
 class Demo {
     private static instance: Demo;
     private constructor() {} // 外部不能访问构造函数
